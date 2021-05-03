e03e28b25734458a09993f688255d9689b6407a9
newkung001 <72070758+newkung001@users.noreply.github.com>
2021-05-03 23:32:04 +0700

    Update covid19_hi5.php

--- covid19_hi5.php
+++ covid19_hi5.php
@@ -6,13 +6,15 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 		<!-- Bootstrap CSS -->
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-		<title>รายงานสถานการณ์ โควิด-19, devbanban.com</title>
+		<title>รางานสรุปเคสโควิด19</title>
 	</head>
 	<body>
 		<?php
 		    @$get_data=file_get_contents('https://covid19.th-stat.com/api/open/cases/sum');
-			$covid19_th=json_decode($get_data);
-/
+			$covid19=json_decode($get_data);
+
 			echo '<pre>';
-			print_r($covid19_th);
+			print_r($covid19);
 			echo '</pre>';
+		?>
+	</body>
